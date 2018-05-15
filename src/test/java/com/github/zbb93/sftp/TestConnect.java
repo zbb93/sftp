2b9386a8bb33522ef049c2d80e0366dd3bf6d847
zbb <zbowen93@gmail.com>
2018-05-14 21:43:23 -0500

    Added connection timeout test.

--- src/test/java/com/github/zbb93/sftp/TestConnect.java
+++ src/test/java/com/github/zbb93/sftp/TestConnect.java
@@ -5,7 +5,6 @@ import com.github.zbb93.sftp.session.auth.*;
 import org.apache.sshd.client.*;
 import org.apache.sshd.client.future.*;
 import org.apache.sshd.client.session.*;
-import org.jetbrains.annotations.*;
 import org.junit.*;
 
 import java.io.*;
@@ -38,27 +37,49 @@ public class TestConnect {
 	/**
 	 * Verifies that we can connect to an SSH server using password authentication.
 	 *
-	 * @throws SSHException if an error occurs while connection to the SSH server.
+	 * @throws SSHException if an error occurs while connecting to the SSH server.
 	 */
 	@Test
 	public void testPasswordConnect() throws SSHException {
-		Authentication passwordAuthentication = AuthenticationFactory.INSTANCE.authenticationFor(SshServerTests.USERNAME,
-				SshServerTests.PASSWORD);
-		ConnectionParameters connectionParameters = buildConnectionParameters(passwordAuthentication);
+		ConnectionParameters connectionParameters = buildPasswordConnectionParameters();
 		Connection connection = ConnectionFactory.INSTANCE.getConnection(connectionParameters);
 		connection.connect();
 		Assert.assertThat("Connection unsuccessful", true, is(connection.isConnected()));
 	}
 
 	/**
-	 * Constructs a ConnectionParameters object to be used to connect to the test SSH server. The Host and Port are
-	 * configured in the ConnectionTest Suite and the Authentication is provided by the calling method.
+	 * This test uses an unroutable IP address to ensure that connection timeout is working as expected.
 	 *
-	 * @param authentication Authentication to be used when connecting to the SSH server.
+	 * @throws SSHException if an unexpected error occurs while connecting to the SSH server.
+	 */
+	@Test
+	public void testConnectionTimeout() throws SSHException {
+		ConnectionParameters connectionParameters = buildPasswordConnectionParameters("10.255.255.1", 1);
+		Connection connection = ConnectionFactory.INSTANCE.getConnection(connectionParameters);
+		try {
+			connection.connect();
+			Assert.fail("Connection did not timeout.");
+		} catch (SSHException e) {
+			Assert.assertThat("Unexpected SSHException occurred.", e.getMessage(), containsString("timeout"));
+		}
+	}
+
+	/**
+	 * Constructs a ConnectionParameters object to be used to connect to the test SSH server. The host, port, username,
+	 * and password are constructed in the test suite.
+	 *
 	 * @return ConnectionParameters Object that can be used to build a Connection to the test SSH server.
 	 */
-	private ConnectionParameters buildConnectionParameters(final @NotNull Authentication authentication) {
-		ConnectionParameters.Builder builder = new ConnectionParameters.Builder(SshServerTests.HOST, authentication, SshServerTests.PORT);
+	private ConnectionParameters buildPasswordConnectionParameters() {
+		return buildPasswordConnectionParameters(SshServerTests.HOST, 0);
+	}
+
+	private ConnectionParameters buildPasswordConnectionParameters(final String host, final int timeout) {
+		Authentication passwordAuthentication = AuthenticationFactory.INSTANCE.authenticationFor(SshServerTests.USERNAME,
+				SshServerTests.PASSWORD);
+		ConnectionParameters.Builder builder = new ConnectionParameters.Builder(host, passwordAuthentication,
+				SshServerTests.PORT);
+		builder.setTimeout(timeout);
 		return builder.build();
 	}
 }
