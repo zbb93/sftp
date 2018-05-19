6830f026f3a209ac18a4a57d1df4fc1592d32c5a
zbb <zbowen93@gmail.com>
2018-05-19 02:35:06 -0500

    Added copyright information

--- src/test/java/com/github/zbb93/sftp/SshServerTests.java
+++ src/test/java/com/github/zbb93/sftp/SshServerTests.java
@@ -1,3 +1,20 @@
+/*
+ * sftp - sftp for java
+ * Copyright (C) 2018  Zac Bowen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.zbb93.sftp;
 
 import com.google.common.collect.Lists;
